0de52bb0d0f97d61ca2539dff247762f42724a90
Niki Theof <93220601+NikiTheofilaktidou@users.noreply.github.com>
2022-03-18 16:06:55 +0200

    Add files via upload

---
+++ DesktopApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using DesktopApp.SortingAlgorithms;
+using System;
+using System.Linq;
+
+namespace DesktopApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDatabase db = new MyDatabase();
+            var shirts = db.TShirts;
+
+            //BubbleSort.OrderBySize(shirts);
+
+            //shirts.BubbleSortOrderBySize();
+            //shirts.BubbleSortOrderByDescendingSize();
+            //shirts.BubbleSortOrderBySizeColorFabric();
+            //shirts.BubbleSortOrderBySizeColorFabric();
+
+            shirts.BubbleSortOrderByDescendingSizeColorFabric();
+
+            //QuickSort.quickSortOrderByColor(shirts,0,shirts.Count-1);
+            //akrivws to idio grammh 18 me grammh 16
+            shirts.quickSortOrderByColor(0, shirts.Count - 1);
+
+
+            foreach (var shirt in shirts)
+            {
+                Console.Write("     "); shirt.Output();
+            }
+        }
+    }
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p = { 1, 1, 3, 4, 4, 8, 9 };
+
+            Array.Sort(p);
+            int index = Array.BinarySearch(p, -10);
+
+            Console.WriteLine(index);
+        }
+    }
+}
